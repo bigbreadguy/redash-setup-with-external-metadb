06eb2192b31e7029bfaa411a1313f600b9602f16
selfie-alookso <100106058+selfie-alookso@users.noreply.github.com>
2022-07-21 21:24:52 +0900

    Update schedule_redis_backup.sh
    Command lines for redis backup schedules.

--- data/schedule_redis_backup.sh
+++ data/schedule_redis_backup.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
 
+helpFunction()
+{
+    echo ""
+    echo "Usage: $0 -C CRON_EXPRESSION"
+    echo -e "\t-CRON_EXPRESSION specifies routine schedule for the operation"
+    exit 1 # Exit script after printing help
+}
+
+setSchedule()
+{
+  while getopts "C:" opt
+  do
+      case "$opt" in
+          C ) CRON_EXPRESSION="$OPTARG" ;;
+          ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+      esac
+  done
+  
+  (crontab -l 2>/dev/null; echo "$CRON_EXPRESSION sudo docker exec -it redash_redis_1 redis-cli -c 'BGSAVE'") | crontab -
+}
+
+setSchedule "$@"
