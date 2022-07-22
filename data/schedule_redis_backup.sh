e9c1f5eac812c344a02715cd57cf534ffa1c8d62
selfie-alookso <100106058+selfie-alookso@users.noreply.github.com>
2022-07-22 09:48:04 +0900

    Update schedule_redis_backup.sh
    Fix indentations

--- data/schedule_redis_backup.sh
+++ data/schedule_redis_backup.sh
@@ -10,15 +10,15 @@ helpFunction()
 
 setSchedule()
 {
-  while getopts "C:" opt
-  do
-      case "$opt" in
-          C ) CRON_EXPRESSION="$OPTARG" ;;
-          ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
-      esac
-  done
+    while getopts "C:" opt
+    do
+        case "$opt" in
+            C ) CRON_EXPRESSION="$OPTARG" ;;
+            ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+        esac
+    done
   
-  (crontab -l 2>/dev/null; echo "$CRON_EXPRESSION sudo docker exec -it redash_redis_1 redis-cli -c 'BGSAVE'") | crontab -
+    (crontab -l 2>/dev/null; echo "$CRON_EXPRESSION sudo docker exec -it redash_redis_1 redis-cli -c 'BGSAVE'") | crontab -
 }
 
 setSchedule "$@"
